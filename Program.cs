9efad8e04bc4d85c0bbaa346cd3f770b0986a5b1
xdiegors <xdiegors@outlook.com>
2022-05-07 12:03:12 -0300

    Primeira versão

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+Clear();
+System.Console.BackgroundColor = ConsoleColor.Red;
+
+CriarBorda();
+
+System.Console.BackgroundColor = ConsoleColor.Black;
+WriteLine("Digite algo");
+string textoDigitado = ReadLine();
+
+WriteLine(textoDigitado);
+
+System.Console.BackgroundColor = ConsoleColor.Red;
+CriarBorda();
+System.Console.BackgroundColor = ConsoleColor.Black;
+
+static void CriarBorda() {
+    
+    Write("+");
+
+    for (int i = 0; i < 90; i++)
+    {
+        Write("-");
+    }
+
+    WriteLine("+");
+}
